c15954e51484ab5e890280a9aa8e01b313cf52e2
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-08-30 17:33:14 +0530

    app.jsx file(react router do functionality added here)

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Collection from './pages/Collection';
+import AboutUs from './pages/AboutUs';
+import Contact from './pages/Contact'
+import Products from './pages/Products'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders';
+import './index.css'; 
+import Navbar from './components/Navbar';
+import { ShoppingContextProvider } from './context/ContextShop';
+
+function App() {
+  return (
+    <>
+    <ShoppingContextProvider>
+      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/collection' element={<Collection/>}/>
+                <Route path='/aboutus' element={<AboutUs/>}/>
+                <Route path='/contact' element={<Contact/>}/>
+                <Route path='/products/:productId' element={<Products/>}/>
+                <Route path='/cart' element={<Cart/>}/>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/place-order' element={<PlaceOrder/>}/>
+                <Route path='/orders' element={<Orders/>}/>
+            </Routes>
+        </BrowserRouter>
+      </div> 
+      </ShoppingContextProvider> 
+    </>
+  );
+}
+
+export default App;
