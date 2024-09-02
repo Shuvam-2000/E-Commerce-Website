38f95b3a4156b3e0ce22d9f89199f8f9bd809b16
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-09-02 16:39:06 +0530

    modified latestproducts section

--- src/components/LatestProducts.jsx
+++ src/components/LatestProducts.jsx
@@ -26,7 +26,7 @@ const LatestProducts = () => {
             {exploreProducts.map((productData) => (
                   <div key={productData._id} className="overflow-hidden border rounded-lg shadow-lg hover:scale-110 transition ease-in-out">
                       <Link to={`/products/${productData._id}`} className="block p-4">
-                            <img src={productData.image} alt={productData.name} className="w-full h-40 sm:mb-5" />
+                            <img src={productData.image} alt={productData.name} className="w-full h-40 sm:mb-5 rounded-lg" />
                             <p className="text-lg font-mono text-[#414141]">{productData.name}</p>
                             <p className="text-[#414141] font-bold">{priceCurrency} {productData.price}</p>
                       </Link>
