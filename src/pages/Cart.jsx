3000088a710f523fc925c0d3d4c83420443a0614
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-09-06 16:40:02 +0530

    cart page development started

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,22 @@
+import { useContext, useEffect, useState } from "react"
+import { ShopContext } from "../context/ContextShop"
 
 const Cart = () => {
+
+  const { priceCurrency, cartItems, deliveryCharge, products } = useContext(ShopContext)
+
+  // all the nessecary states
+  const [cartData, setCartData] = useState([])
+
+  // useEffect(() => {
+    
+  //   const tempCartData = [];
+  //   for(const data in cartItems)
+
+  //   },[cartItems])
+
   return (
-    <div>
+    <div className="">
       
     </div>
   )
